730c2ff9866a680c120e336fea2523e90d7a69d7
unknown <tatawedha@gmail.com>
2023-02-08 09:06:31 +0700

    ip

--- src/ipbackend.js
+++ src/ipbackend.js
@@ -1,5 +1,6 @@
-// let ipBackend = 'http://besimpelmas.salatiga.go.id/';
-let ipBackend = 'http://localhost:8804/';
+// let ipBackend = 'https://besimpelmas.salatiga.go.id/';
+// let ipBackend = 'http://localhost:8804/';
+let ipBackend = 'https://simpelmas.salatiga.go.id/api/';
 
 
 module.exports = ipBackend
