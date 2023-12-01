38b67c0c1fac670a1de679abfaf4c6607e8e72ea
DANANG SR <danangsukoraharjo@gmail.com>
2023-12-01 17:10:18 +0700

    update all page cetak opd

--- src/ipbackend.js
+++ src/ipbackend.js
@@ -1,6 +1,6 @@
 // let ipBackend = 'https://besimpelmas.salatiga.go.id/';
-let ipBackend = 'http://localhost:8804/';
-// let ipBackend = 'http://simpelmas.salatiga.go.id/api/';
+// let ipBackend = 'http://localhost:8804/';
+let ipBackend = 'http://simpelmas.salatiga.go.id/api/';
 
 
 module.exports = ipBackend
